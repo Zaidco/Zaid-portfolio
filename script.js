1ee1b298d8bbfd2e399d1b3fed34b8bb3568980a
Zaid <zibro83@gmail.com>
2026-04-21 23:33:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const items = document.querySelectorAll('.reveal');
 
 const io = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
-    if (entry.isIntersecting) entry.target.classList.add('show');
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
   });
-}, { threshold: 0.14 });
+}, { threshold: 0.1 });
 
 items.forEach(el => io.observe(el));
